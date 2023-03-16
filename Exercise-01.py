20cbc99f7bdce41aa5df9441cd63ff85d3df07c9
Jagad Surya Andromeda <jagadsuryaandromeda@gmail.com>
2023-03-16 11:09:00 +0700

    Menambah file

---
+++ Exercise-01.py
@@ -0,0 +1,11 @@
+print("Nama : Jagad Surya Andromeda")
+print("NIM  : I0322056")
+print("Asal : Ngawi, Jawa Timur\n")
+
+print("=============================\n")
+
+print("Kabupaten Ngawi adalah terletak di perbatasan Jawa Timur dengan Jawa Tengah")
+print("Kabupaten Ngawi adalah salah satu dari tiga kabupaten yang terletak di kaki gunung Lawu")
+print("Ngawi memiliki banyak sarana untuk berlibur")
+print("Ngawi juga terkenal akan hutan yang hijau serta kaya akan budi daya bambu")
+print("Harapan  setelah lulus dari Teknik Industri adalah saya ingin bekerja sesuai bidang perkuliahan saya yakni Industrial")
